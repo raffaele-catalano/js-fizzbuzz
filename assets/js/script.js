7a9e2adddcf72ad62b53699397fc54d813149b6a
raffaele-catalano <raf.catalano@gmail.com>
2023-03-22 18:06:20 +0100

    added entire functioning structure but w/ "accrocchio"

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+const container = document.querySelector('.container')
+    console.log(container);
+
+for (let i = 1; i <= 100; i++) {
+    console.log(i);
+
+    const box = document.createElement('div')
+
+    box.classList.add('box')
+
+    if (!(i % 3)){
+        box.classList.add('fizz')
+        box.innerHTML = "Fizz"
+    } else if (!(i % 5)){
+        box.classList.add('buzz')
+        box.innerHTML = "Buzz"
+    } 
+
+    if (!(i % 15)) {
+        box.classList.add('fizz' + 'buzz')
+        box.innerHTML = "FizzBuzz"
+    }
+
+    // box.append(i)
+
+    container.append(box)
+    }
+
+
+
+
+
