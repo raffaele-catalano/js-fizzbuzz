95c61df5bc51d7b417be79b72f12c97418aeb259
raffaele-catalano <raf.catalano@gmail.com>
2023-03-23 10:12:02 +0100

    added let-output=i instead of innerHTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,28 +5,31 @@ const container = document.querySelector('.container')
 for (let i = 1; i <= 100; i++) {
     console.log(i);
     
-
     const box = document.createElement('div')
-
     box.classList.add('box')
+    console.log(box);
+    
+    let output = i;
     
     if (!(i % 15)){
         box.classList.add('fizzbuzz')
-        box.innerHTML = "FizzBuzz"
+        output = "FizzBuzz"
     } else if (!(i % 3)){
         box.classList.add('fizz')
-        box.innerHTML = "Fizz"
+        output = "Fizz"
     } else if (!(i % 5)){
         box.classList.add('buzz')
-        box.innerHTML = "Buzz"
-    } else {
-        box.classList.add(i)
-        box.innerHTML = i
+        output = "Buzz"
+    } 
         
-    }
+
+    box.append(output)
 
     container.append(box)
+
     }
+
+    
 
 
 
